a23370035961940bcec11b0826e3be7c78743a34
AmirhosseinAhani <amir.ahani1377@gmail.com>
2025-08-09 02:01:36 +0330

    Initial Commit

---
+++ AkakceCaseTests/AkakceCaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AkakceCaseTests.swift
+//  AkakceCaseTests
+//
+//  Created by Amir Ahani on 9.08.2025.
+//
+
+import Testing
+@testable import AkakceCase
+
+struct AkakceCaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
